9bafb3830f6f80a4a89f5c15bfaf0a87fcd1d77d
Victor Allen <vamuigua@gmail.com>
2023-10-05 17:18:56 +0300

    feat(auth): add ability to login user and create access token

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post('auth/register', Auth\RegisterController::class);
+Route::post('auth/login', Auth\LoginController::class);

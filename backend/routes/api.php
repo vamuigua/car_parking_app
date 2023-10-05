e9e48f520a7c1d615b78fbdcad608736ac8fd82f
Victor Allen <vamuigua@gmail.com>
2023-10-05 17:42:49 +0300

    feat(auth): add sanctum authenticated routes
    add route for showing user profile info
    add route for updating user name, email & password
    add route for logging out user & deleting user's current access token

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,24 +1,14 @@
 <?php
 
 use \App\Http\Controllers\Api\V1\Auth;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('profile', [Auth\ProfileController::class, 'show']);
+    Route::put('profile', [Auth\ProfileController::class, 'update']);
+    Route::put('password', Auth\PasswordUpdateController::class);
+    Route::post('auth/logout', Auth\LogoutController::class);
+});

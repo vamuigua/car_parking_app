84a935ba345e01a371fe4cef7c515ea57845d9f8
Victor Allen <vamuigua@gmail.com>
2023-10-07 17:15:01 +0300

    feat(parking): add ability to start and stop vehicle parking

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,18 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\Auth;
 use App\Http\Controllers\Api\V1\ZoneController;
+use App\Http\Controllers\Api\V1\ParkingController;
 use App\Http\Controllers\Api\V1\VehicleController;
 
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
+    // Auth routes
     Route::get('profile', [Auth\ProfileController::class, 'show']);
     Route::put('profile', [Auth\ProfileController::class, 'update']);
     Route::put('password', Auth\PasswordUpdateController::class);
     Route::post('auth/logout', Auth\LogoutController::class);
 
+    // Vehicle routes
     Route::apiResource('vehicles', VehicleController::class);
 
+    // Zone routes
     Route::get('zones', [ZoneController::class, 'index']);
+
+    // Parking routes
+    Route::post('parkings/start', [ParkingController::class, 'start']);
+    Route::get('parkings/{parking}', [ParkingController::class, 'show']);
+    Route::put('parkings/{parking}', [ParkingController::class, 'stop']);
 });

aecc9d63dea2f1ea350b091ec1075e5bba569056
Victor Allen <vamuigua@gmail.com>
2024-03-20 08:17:23 +0300

    chore: fix warning issue with depricated tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,12 @@
 
 const colors = require('tailwindcss/colors')
 
+delete colors['lightBlue']
+delete colors['warmGray']
+delete colors['trueGray']
+delete colors['coolGray']
+delete colors['blueGray']
+
 export default {
   content: ['./src/**/*.{vue,js}'],
   theme: {

16866f7af418d9332f88ce8db81be0763e29898f
Victor Allen <vamuigua@gmail.com>
2024-03-20 21:23:07 +0300

    feat(frontend): add validation error UI on register form

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,19 @@
 import '@/bootstrap.js'
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ValidationError from '@/components/ValidationError.vue'
+
+import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.component('ValidationError', ValidationError)
 
 app.mount('#app')

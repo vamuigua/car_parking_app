bc33b8b0cbf2662799f4eaaed3fa25f7870a9897
Victor Allen <vamuigua@gmail.com>
2024-03-20 08:38:22 +0300

    UI(frontend): add registration page with sign-up form

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: import('../views/HomeView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/Auth/RegisterView.vue')
     }
   ]
 })
